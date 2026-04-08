56ce9296178e9049ba41cb19fbaef9a24f89075d
Alex <cuellaralex314@gmail.com>
2026-04-08 16:22:52 -0300

    nnn

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ function saludar() {
 window.onload = function() {
     document.body.style.opacity = "1";
 }
+
+const elementos = document.querySelectorAll('.hidden');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+elementos.forEach(el => observer.observe(el));
